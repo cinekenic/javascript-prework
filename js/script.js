50a13b81baed0d40c3884a3807f27e106b70d884
Piotr-Trzcinski <cinekenic@go2.pl>
2021-05-07 11:49:23 +0200

    Add logic and score

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ if (randomNumber == 1) {
   computerMove = "kamień";
 } else if (randomNumber == 2) {
   computerMove = "papier";
-} else {
+} else if (randomNumber == 3) {
   computerMove = "nożyce";
 }
+
+console.log(computerMove);
 
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce");
 
@@ -20,6 +22,20 @@ if (playerInput == "1") {
   playerMove = "kamień";
 } else if (playerInput == "2") {
   playerMove = "papier";
-} else {
+} else if (playerInput == "3") {
   playerMove = "nożyce";
+}
+
+console.log(playerMove);
+
+if (computerMove == "kamień" && playerMove == "papier") {
+  console.log("Ty wygrywasz!");
+} else if (computerMove == "papier" && playerMove == "nożyce") {
+  console.log("Ty wygrywsz!");
+} else if (computerMove == "nożyce" && playerMove == "kamień") {
+  console.log("Ty wygrywasz!");
+} else if (computerMove == playerMove) {
+  console.log("Remis");
+} else {
+  console.log("Przegrałeś!");
 }
