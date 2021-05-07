a4369f472c4f4a34b7155d76cd25479565d19ee1
Piotr-Trzcinski <cinekenic@go2.pl>
2021-05-07 11:11:47 +0200

    Add randomNumber computerNumber playerInput and playerMove

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+let randomNumber = Math.random() * 3 + 1;
+console.log("Wylosowana liczba to:" + randomNumber);
 
-printMessage(
-  "Zagrałem" + computerMove + "! Jeśli Twój ruch to paier, to wygrywasz!"
-);
+let computerMove = "Nieznany ruch";
+if (randomNumber == 1) {
+  computerMove = "kamień";
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+} else {
+  computerMove = "nożyce";
+}
+
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce");
+
+console.log("Gracz wybrał" + playerInput);
+
+let playerMove = "Nieznany ruch";
+
+if (playerInput == "1") {
+  playerMove = "kamień";
+} else if (playerInput == "2") {
+  playerMove = "papier";
+} else {
+  playerMove = "nożyce";
+}
